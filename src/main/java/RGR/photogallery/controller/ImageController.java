f484cbe0347ac519b6501317b03c54b43bacac1c
Olga-Kamaeva <63469157+OlgaKamaeva@users.noreply.github.com>
2021-06-10 16:24:38 +0400

    Удаление фото

--- src/main/java/RGR/photogallery/controller/ImageController.java
+++ src/main/java/RGR/photogallery/controller/ImageController.java
@@ -2,8 +2,10 @@ package RGR.photogallery.controller;
 
 import RGR.photogallery.domain.Album;
 import RGR.photogallery.domain.Image;
+import RGR.photogallery.domain.User;
 import RGR.photogallery.repository.AlbumRepository;
 import RGR.photogallery.repository.ImageRepository;
+import RGR.photogallery.repository.UserRepository;
 import RGR.photogallery.service.AlbumService;
 import RGR.photogallery.service.CommentService;
 import RGR.photogallery.service.ImageService;
@@ -11,6 +13,8 @@ import RGR.photogallery.service.ImageServiceDomain;
 import RGR.photogallery.util.ProfileImageService;
 import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -39,6 +43,9 @@ public class ImageController {
     @Autowired
     private AlbumRepository albumRepository;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping("/addPhoto/{albumId}")
     public ModelAndView addImageAtAlbum(@RequestParam("file") MultipartFile file, @PathVariable(name = "albumId") Long albumId, ModelAndView model, @RequestParam String title, @RequestParam String tags) {
         System.out.println(albumId + title + file.getOriginalFilename() + tags);
@@ -58,9 +65,21 @@ public class ImageController {
 
     @GetMapping("/image_at_album/{imageId}")
     public ModelAndView imagePage(ModelAndView model, @PathVariable(name = "imageId") Long imageId) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        Optional<User> user = userRepository.findByEmail(auth.getName());
+        model.addObject("user", user.get());
         Image image = imageRepository.findById(imageId).get();
+        Album album = albumRepository.findById(image.getAlbumId()).get();
         model.addObject("image", image);
-        model.setViewName("/");
+        model.addObject("album", album);
+        model.setViewName("imagePage");
+        return model;
+    }
+
+    @GetMapping("/deletePhoto/{id}")
+    public ModelAndView deletePhoto(ModelAndView model, @PathVariable(name = "id") Long id) {
+        imageService.deletePhoto(id);
+        model.setViewName("redirect:/user/profile");
         return model;
     }
 
